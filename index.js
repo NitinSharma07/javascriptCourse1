38aa08c90dc850cb35c5173b5e40d5cff3b5377e
Nitin <nitinsharma01001@gmail.com>
2023-01-13 20:25:15 +0530

    some more basic

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 //document.getElementById("count-el").innerText = 5
 
-let myAge = 20
-let humanDogRatio = 7
-myDogAge = myAge * humanDogRatio 
-console.log(myDogAge)
+let bonusPoints  =50
+console.log(bonusPoints)
+bonusPoints += 50
+console.log(bonusPoints)
+bonusPoints -= 75
+console.log(bonusPoints)
+bonusPoints += 45
+console.log(bonusPoints)
+
+
+
 
