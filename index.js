7aae960ba4ad9357787e26316fc5af8d61ad8cda
Nitin <nitinsharma01001@gmail.com>
2023-01-13 21:06:22 +0530

    little change

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //document.getElementById("count-el").innerText = 5
+let countEl = document.getElementById("count-el")
 let count = 0
 function increment() {
     count += 1
-    document.getElementById("count-el").innerText = count
+    countEl.innerText = count
     console.log("clicked", count)
 }
 
