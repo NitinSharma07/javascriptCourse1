ad7f8add2e278542fb762f0f7195df9c0db122b8
Nitin <nitinsharma01001@gmail.com>
2023-01-13 21:03:03 +0530

    did some changes to the increment button.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 //document.getElementById("count-el").innerText = 5
+let count = 0
 function increment() {
-    console.log("The button was clicked")
+    count += 1
+    document.getElementById("count-el").innerText = count
+    console.log("clicked", count)
 }
-
-
-
-
 
