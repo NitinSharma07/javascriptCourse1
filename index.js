72b348401e529b6393a79afde91096bbdc928791
Nitin <nitinsharma01001@gmail.com>
2023-01-13 20:19:05 +0530

    some more basic

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 //document.getElementById("count-el").innerText = 5
 
-let count = 0
-console.log(count)
-
-let my_Age = 20
-console.log(my_Age)
+let myAge = 20
+let humanDogRatio = 7
+myDogAge = myAge * humanDogRatio 
+console.log(myDogAge)
 
