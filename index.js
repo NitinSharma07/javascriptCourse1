78ab5a8fca27bb8cd823b90157513346ca5549c0
Nitin <nitinsharma01001@gmail.com>
2023-01-13 21:17:17 +0530

    adding a save button

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 //document.getElementById("count-el").innerText = 5
-let countEl = document.getElementById("count-el")
 let count = 0
 function increment() {
     count += 1
-    countEl.innerText = count
-    console.log("clicked", count)
+    document.getElementById("count-el").innerText = count
+
 }
 
+
+function save() {
+    console.log(count)
+}
