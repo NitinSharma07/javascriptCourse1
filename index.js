19e49d7d0f487e1dda3719a04691731a71cdec93
Nitin <nitinsharma01001@gmail.com>
2023-01-14 21:54:10 +0530

    blackjack litlle version

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
-let count = 0
-function increment() {
-    count += 1
-    countEl.innerText = count
-}
-function save(){
-    let record = count + " - " 
-    saveEl.textContent += record
-    count = 0
-    countEl.textContent = count
-}
+let firstCard = 10
+let secondCard = 9
+let sum = firstCard + secondCard 
 
+if (sum < 21) {
+    console.log("Do you want to draw a new card?")
+}else if (sum === 21) {
+    console.log("wohoo! You've got Blackjack!")
+}else{
+    console.log("You're out of the game!")
+}
