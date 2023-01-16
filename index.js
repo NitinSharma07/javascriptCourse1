83e98296a5c85e6dd99b4f9edfafdb1f841da02b
Nitin <nitinsharma01001@gmail.com>
2023-01-16 13:49:52 +0530

    some more changes to the blackjack game.

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 let firstCard = 10
 let secondCard = 9
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard 
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card?")
-}else if (sum === 21) {
-    console.log("wohoo! You've got Blackjack!")
-}else{
-    console.log("You're out of the game!")
+let message = ""
+let hasBlackjack = false
+let isAlive = true
+
+messageEl = document.getElementById("message-el")
+// sumEl = document.getElementById("sum-el")
+sumEl = document.querySelector("#sum-el")
+cardEl = document.querySelector("#card-el")
+
+function newCard() {
+    let newCard  = 2
+    console.log("Drawing a new card from the deck.")
+    sum += newCard
+    renderGame()
 }
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    // cardEl.textContent = "Cards: "
+    // sumEl.textContent = "Sum: "
+    cardEl.textContent = "Cards: " +  cards[0] + ", " + cards[1] 
+    sumEl.textContent = "Sum: "  +  sum
+    if (sum < 21) {    
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "wohoo! You've got Blackjack!"
+        hasBlackjack = true    
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+
+
+
+
+
