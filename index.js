b55d0be8f76c504cf641cd94809e7494bae9ae8e
Nitin <nitinsharma01001@gmail.com>
2023-01-19 16:17:23 +0530

    opening a link in a new tab using target =_blank

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-let myLeads  = ["a", "b", "c"]
+/* let myLeads  = []
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
 const ulEl = document.querySelector("#ul-el")
 
-
 inputBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    renderLeads()
+    inputEl.value =""
 }) 
 
-let listItems = []
-for (let i = 0; i < myLeads.length; i += 1) {
-    
-    const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    ulEl.append(li)
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i += 1) {
+        // listItems += "<li><a target= '_blank' href =" + myLeads[i]+">"+ myLeads[i] + "</a></li>"
+        listItems += `<li>
+            <a target= _blank href = ${myLeads[i]}>
+                ${myLeads[i]}
+            </a>
+        </li>`
+    }
+    ulEl.innerHTML = listItems
 }
-
-
-
-
+ */
 
