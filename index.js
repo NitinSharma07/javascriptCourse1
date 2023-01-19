63720cf90b1b830ee071eee13c0ef9383f54dc40
Nitin <nitinsharma01001@gmail.com>
2023-01-19 15:07:30 +0530

    Here i learnt something about innerHTML

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 /* let myLeads  = []
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
+const ulEl = document.querySelector("#ul-el")
+
 
 inputBtn.addEventListener('click', function() {
-    console.log("Button clicked!")
-})
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+}) */
+for (let i = 0; i < myLeads.length; i += 1) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
 
 
- */
-
+buyBtn = document.querySelector("#buy-btn")
+buyBtn.innerHTML = "<button>BUY!</button>"
