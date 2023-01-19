0add0895275591a22565598e7c4776a86369581e
Nitin <nitinsharma01001@gmail.com>
2023-01-19 15:13:41 +0530

    doing a little more.

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ const ulEl = document.querySelector("#ul-el")
 inputBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
-}) */
+}) 
 for (let i = 0; i < myLeads.length; i += 1) {
     ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
 }
 
-
+*/
 buyBtn = document.querySelector("#buy-btn")
 buyBtn.innerHTML = "<button>BUY!</button>"
+
+buyBtn.addEventListener ('click', function() {
+    buyBtn.innerHTML = "<p> Thank you for buying </p>"
+})
