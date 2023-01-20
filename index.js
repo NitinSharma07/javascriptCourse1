7c384e99864876288137e077b7e3368d460772a3
Nitin <nitinsharma01001@gmail.com>
2023-01-20 18:34:01 +0530

    learning about local storage and how to setItem() and getItem and then how to clear() the console.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-/* let myLeads  = []
+let myLeads  = []
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
 const ulEl = document.querySelector("#ul-el")
+
+// localStorage.setItem("name", "Nitin Sharma")
+console.log(localStorage.getItem("name"))
+localStorage.clear()
+
+
 
 inputBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value)
@@ -12,7 +18,6 @@ inputBtn.addEventListener('click', function() {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i += 1) {
-        // listItems += "<li><a target= '_blank' href =" + myLeads[i]+">"+ myLeads[i] + "</a></li>"
         listItems += `<li>
             <a target= _blank href = ${myLeads[i]}>
                 ${myLeads[i]}
@@ -21,5 +26,6 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems
 }
- */
+
+ 
 
