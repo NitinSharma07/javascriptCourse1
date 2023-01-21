52d216b4412525ef1eb6e0c7f017e01d18510966
Nitin <nitinsharma01001@gmail.com>
2023-01-21 12:05:50 +0530

    done with the project of chrome extension

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
 let myLeads  = []
+let oldLeads = []
 const inputEl = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
 const ulEl = document.querySelector("#ul-el")
-let myLocalLeads = JSON.parse(localStorage.getItem("myLeads"))
-console.log(myLocalLeads)
+const myLocalLeads = JSON.parse(localStorage.getItem("myLeads"))
+const deleteBtn = document.querySelector("#delete-btn")
+const tabBtn = document.querySelector("#tab-btn")
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    ulEl.innerHTML = ""
+    myLeads = []
+})
+
+
+tabBtn.addEventListener('click', function() {
+    // chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
+    // })
+    chrome.tabs.query({active: true, currentWindow : true}, function(tab) {
+        myLeads.push(tab[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+    
+
+})
+
+if (myLocalLeads) {
+    myLeads = myLocalLeads
+    render(myLeads)
+}
+
+
 
 inputBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value)
     inputEl.value =""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
+    render()
     console.log(localStorage.getItem("myLeads"))
 }) 
 
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i += 1) {
+    for (let i = 0; i < leads.length; i += 1) {
         listItems += `<li>
-            <a target= _blank href = ${myLeads[i]}>
-                ${myLeads[i]}
+            <a target= _blank href = ${leads[i]}>
+                ${leads[i]}
             </a>
         </li>`
     }
     ulEl.innerHTML = listItems
 }
-
- 
 
